142411e4d4c1451990679a07934575943426634e
jiseon723 <dighxxx12@gmail.com>
2025-07-14 15:21:05 +0900

    5단계

--- src/main/java/com/ll/APP.java
+++ src/main/java/com/ll/APP.java
@@ -1,5 +1,6 @@
 package com.ll;
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class APP {
@@ -10,6 +11,7 @@ public class APP {
 
     public void run () {
         int lastId = 1;
+        List<Article> articleList = new ArrayList<>();
         System.out.println("== 게시판 앱 ==");
 
         while (true){
@@ -20,12 +22,23 @@ public class APP {
                 break;
             } else if (command.equals("등록")){
                 System.out.print("제목: ");
-                String title = sc.nextLine();
+                String subject = sc.nextLine();
                 System.out.print("내용: ");
                 String content = sc.nextLine();
 
+                Article article  = new Article(lastId, subject, content);
+                articleList.add(article);
+
                 System.out.printf("%d번 게시글이 등록되었습니다.\n", lastId);
                 lastId++;
+            } else if (command.equals("목록")){
+                System.out.println("번호 / 제목 / 내용");
+                System.out.println("----------------------");
+            }
+
+            for (int i = 0; i < articleList.size(); i++){
+                Article article = articleList.get(i);
+                System.out.printf("%d / %s / %s\n",article.getId(),article.getSubject(),article.getContent());
             }
         }
     }
--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         new APP(sc).run();
-
         sc.close();
     }
 }
